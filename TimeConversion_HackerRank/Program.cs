1b04108a814c9753b0a79cf620306312f947f19f
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-28 15:04:07 +0400

    Добавьте файлы проекта.

---
+++ TimeConversion_HackerRank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion_HackerRank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "12:01:00AM";
+            string x = timeConversion(s);
+            Console.WriteLine(x);
+        }
+
+        public static string timeConversion(string s)
+        {
+            DateTime dt = DateTime.Parse(s);
+            return dt.ToString("HH:mm:ss");
+        }
+    }
+}
